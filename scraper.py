ae6710d907a8d5e84457690731bfafb23fde5d45
the3rdPoliceman <gabbymoore@gmail.com>
2023-06-05 20:33:36 +0200

    tidied code and made run from config files

--- scraper.py
+++ scraper.py
@@ -1,117 +1,144 @@
+import argparse
+import json
+import os.path
 import re
 from math import ceil
-
+import logging
 import requests
 from bs4 import BeautifulSoup
 
+# Constants
+HOMEGATE_PREFIX = "https://www.homegate.ch"
+RESULT_CLASS = "ResultListHeader_locations_3uuG8"
 
-def get_page_list(url, placeholder, start_number, end_number):
+# Set up logging
+logging.basicConfig(level=logging.INFO)
+
+
+def get_page_list(url_template, start_number, end_number):
+    """Generates a list of page URLs based on a template and a range of page numbers."""
     page_list = []
-    for page_number in range(start_number, end_number+1):
-        url_with_page = url.replace(placeholder, str(page_number))
+    for page_number in range(start_number, end_number + 1):
+        url_with_page = url_template.format(page=page_number)
         page_list.append(url_with_page)
-
     return page_list
 
 
 def get_encoding(soup):
+    """Extracts the encoding from a BeautifulSoup object."""
     encoding = soup.meta.get('charset')
-    if encoding == None:
+    if encoding is None:
         encoding = soup.meta.get('content-type')
-    if encoding == None:
+    if encoding is None:
         encoding = soup.meta.get('content')
-
     return encoding
 
 
 def grab_page_content(url):
-    page = requests.get(url)
-    contents = page.content
-    soup = BeautifulSoup(contents, 'html.parser')
-
+    """Requests a URL and returns its content and encoding."""
+    try:
+        page = requests.get(url)
+        contents = page.content
+        soup = BeautifulSoup(contents, 'html.parser')
+    except requests.RequestException as e:
+        logging.error(f"Failed to grab page content: {e}")
+        return None, None
     return contents, get_encoding(soup)
 
 
 def get_property_links_from_page(contents):
+    """Extracts property links from a page's HTML content."""
     soup = BeautifulSoup(contents, 'html.parser')
     pattern = re.compile(r'/mieten/\d{5,}')
     all_property_links = soup.find_all('a', href=pattern)
-    property_links_list = list()
+    return [property_link["href"].split("#")[0] for property_link in all_property_links]
+
+
+def get_page_count(postcode, distance, url_template):
+    """Determines the number of pages of results for a given postcode and distance."""
+    url = url_template.format(postcode=postcode, distance=distance, page=1)
+    contents, _ = grab_page_content(url)
+    if contents is None:
+        return 0
+    soup = BeautifulSoup(contents, 'html.parser')
+    result_count = soup.find('span', attrs={"class": RESULT_CLASS}).text
+    match = re.match(r"(\d+) Treffer", result_count)
+    results = int(match.group(1))
+    return ceil(results/20)
+
+
+def load_json(file_name):
+    """Loads a JSON file or returns an empty list if the file does not exist."""
+    if os.path.isfile(file_name):
+        with open(file_name, 'r') as f:
+            return json.load(f)
+    return []
+
+
+def write_json(file_name, data):
+    """Writes data to a JSON file."""
+    with open(file_name, 'w') as f:
+        json.dump(data, f)
+
+
+def main():
+    """Main function."""
+    # Parse command line arguments
+    parser = argparse.ArgumentParser(description='Process some integers.')
+    parser.add_argument('config', type=str, help='A JSON configuration file')
+    args = parser.parse_args()
+
+    # Load configuration
+    with open(args.config, 'r') as f:
+        config = json.load(f)
+
+    url_template = config['url_template']
+    postcodes = config['postcodes']
+    distances = config['distances']
+    search_terms = config['search_terms']
+    possible_file = config['possible_file']
+    rejected_file = config['rejected_file']
+
+    rejected_urls = set(load_json(rejected_file))
+    possible_urls = set(load_json(possible_file))
+
+    all_page_list = []
+    for postcode in postcodes:
+        for distance in distances:
+            logging.info(f"Searching postcode {postcode}, distance = {distance}")
+            pages = get_page_count(postcode, distance, url_template)
+            if pages > 0:
+                page_list = get_page_list(url_template.replace('{postcode}', postcode).replace('{distance}', distance),
+                                          1, pages)
+                all_page_list.extend(page_list)
+
+    all_property_links = []
+    for page in all_page_list:
+        logging.info(f"Getting {page}")
+        content, _ = grab_page_content(page)
+        if content is not None:
+            property_links_from_page = get_property_links_from_page(content)
+            all_property_links.extend([link for link in property_links_from_page if link not in all_property_links])
 
     for property_link in all_property_links:
-        property_href = property_link["href"].split("#")[0]
-        if property_href not in property_links_list:
-            property_links_list.append(property_href)
+        property_address = HOMEGATE_PREFIX + property_link
+        if property_address in rejected_urls:
+            logging.info(f"Skipping rejected URL: {property_address}")
+            continue
 
-    return property_links_list
+        logging.info(f"Getting page for {property_address}")
+        property_page_content, _ = grab_page_content(property_address)
+        if property_page_content is not None:
+            property_page_content = property_page_content.decode("utf-8")
 
+            if any(term in property_page_content for term in search_terms):
+                possible_urls.add(property_address)
+            else:
+                rejected_urls.add(property_address)
 
-def get_page_count(postcode, distance):
-    postcode_template = "https://www.homegate.ch/mieten/parkplatz-garage/plz-%/trefferliste?be=!".replace("%", postcode).replace("!", distance)
-    page = requests.get(postcode_template)
-    contents = page.content
-    soup = BeautifulSoup(contents, 'html.parser')
-    result_count = soup.find('span', attrs={"class": "ResultListHeader_locations_3uuG8"}).text
-
-    match = re.match(r"(\d+) Treffer", result_count)
-
-    results = int(match.group(1))
-    pages = ceil(results/20)
-
-    print("found " + str(pages) + " pages for " + postcode)
-    return pages
+    write_json(possible_file, list(possible_urls))
+    write_json(rejected_file, list(rejected_urls))
 
 
-#postcode_list_full = ["8038", "8802", "8803", "8800", "8942", "8810", "8804", "8820", "8805", "8832", "8806", "8807", "8805", "8815", "8041", "8134", "8045", "8055", "8008", "8702", "8700", "8703", "8704", "8706", "8707", "8708", "8712", "8713", "8714", "8616", "8617", "8132", "8124"]
-#distance_list_full = ["1000", "2000", "5000", "8000", "10000", "15000"]
-
-postcode_list_full = ["8008"]
-distance_list_full = ["1000"]
-
-postcode_list = postcode_list_full
-distance_list = distance_list_full
-
-all_page_list = list()
-for postcode in postcode_list:
-    for distance in distance_list:
-        print("searching postcode " + postcode + ", distance = " + distance)
-        pages = get_page_count(postcode, distance)
-        if pages > 0:
-            page_list = get_page_list("https://www.homegate.ch/mieten/parkplatz-garage/plz-#/trefferliste?ep=%&be=!".replace("#", postcode).replace("!", distance), "%", 1, pages)
-            for page in page_list:
-                all_page_list.append(page)
-
-
-all_property_links = list()
-for page in all_page_list:
-    print("getting " + page)
-    content, encoding = grab_page_content(page)
-    property_links_from_page = get_property_links_from_page(content)
-    for property_link in property_links_from_page:
-        if property_link not in all_property_links:
-            all_property_links.append(property_link)
-
-
-possible_places = set()
-for index,property_link in enumerate(all_property_links):
-    homegate_prefix = "https://www.homegate.ch"
-    property_address = homegate_prefix + property_link
-    print("getting page for " + property_address)
-    property_page_content, encoding = grab_page_content(property_address)
-    property_page_content = property_page_content.decode("utf-8")
-
-    if "wohnwagen" in property_page_content or "wohnmobile" in property_page_content:
-        possible_places.add(property_address)
-        filename = "possible_" + str(index) + ".txt"
-        with open(filename, "w") as text_file:
-            text_file.write(property_address + " " + property_page_content)
-    else:
-        filename = "rejected_" + str(index) + ".txt"
-        with open(filename, "w") as text_file:
-            text_file.write(property_address + " " + property_page_content)
-
-
-for possible_place in possible_places:
-    print("possible place" + possible_place)
-
-
+if __name__ == "__main__":
+    main()
